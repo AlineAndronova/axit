18a560f3042b71d08442461bc8df9b9b0c96f99c
Alina Andronova <alineandronova@gmail.com>
2017-05-14 10:54:24 +0300

    Tabs added, page scroll effect added

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,30 @@
-;(function(){
+;(function($){
 	'use strict';
-	
-})();
+
+	$('a[href*="#"]').on('click', function() {
+		event.preventDefault();
+
+		$('body').animate({
+			scrollTop: $( $(this).attr('href') ).offset().top
+		}, 600);
+
+	});
+
+	$('.ba-tabs__link').click(function(event) {
+		$('.ba-tabs__link').each(function() {
+			$(this).removeClass('ba-tabs__link--active')
+		});
+
+		$('.ba-tabs__content').each(function() {
+			$(this).removeClass('ba-tabs__content--active')
+		});
+
+		var $tabBox = $(this);
+		$tabBox.addClass('ba-tabs__link--active');
+
+		var tabId = '#ba-tab-' + $tabBox.data('tab');
+		var $tabContentBlock = $(tabId);
+		$tabContentBlock.addClass('ba-tabs__content--active')
+	});
+
+})(jQuery);
